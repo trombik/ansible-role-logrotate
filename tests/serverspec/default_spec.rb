7bf9207b349a737a62e3da1f72e7279c0c255dcb
Tomoyuki Sakurai <y@trombik.org>
2021-10-17 11:32:06 +0700

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -2,57 +2,41 @@ require "spec_helper"
 require "serverspec"
 
 package = "logrotate"
-service = "logrotate"
-config  = "/etc/logrotate/logrotate.conf"
-user    = "logrotate"
-group   = "logrotate"
-ports   = [PORTS]
-log_dir = "/var/log/logrotate"
-db_dir  = "/var/lib/logrotate"
-
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/logrotate.conf"
-  db_dir = "/var/db/logrotate"
-end
+config_dir = case os[:family]
+             when "freebsd"
+               "/usr/local/etc"
+             else
+               "/etc"
+             end
+config  = "#{config_dir}/logrotate.conf"
+conf_d_dir = "#{config_dir}/logrotate.d"
+default_user = "root"
+default_group = case os[:family]
+                when /bsd/
+                  "wheel"
+                else
+                  "root"
+                end
 
 describe package(package) do
   it { should be_installed }
 end
 
 describe file(config) do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("logrotate") }
+  it { should be_mode 644 }
+  its(:content) { should match Regexp.escape("Managed by ansible") }
 end
 
-describe file(log_dir) do
+describe cron do
+  it { should have_entry "0 * * * * logrotate #{config} >/dev/null" }
+end
+
+describe file(conf_d_dir) do
   it { should exist }
+  it { should be_directory }
   it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/logrotate") do
-    it { should be_file }
-  end
-end
-
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
 end
